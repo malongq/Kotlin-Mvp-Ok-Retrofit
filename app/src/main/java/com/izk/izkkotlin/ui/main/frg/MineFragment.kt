aaa940d08fafe4b8b2f168b4e6579e0398e42772
malong <malong@100tal.com>
2020-12-01 16:38:39 +0800

    我的页面开始装饰

--- app/src/main/java/com/izk/izkkotlin/ui/main/frg/MineFragment.kt
+++ app/src/main/java/com/izk/izkkotlin/ui/main/frg/MineFragment.kt
@@ -1,11 +1,10 @@
 package com.izk.izkkotlin.ui.main.frg
 
-import android.os.Bundle
-import android.view.View
 import com.izk.izkkotlin.R
 import com.izk.izkkotlin.base.BaseFragment
 import com.izk.izkkotlin.ui.main.presenter.HomePresenter
 import com.izk.izkkotlin.ui.main.view.HomeView
+import kotlinx.android.synthetic.main.fragment_mine.*
 
 /**
  *  Created by malong on 2020/11/24
@@ -14,6 +13,7 @@ import com.izk.izkkotlin.ui.main.view.HomeView
 class MineFragment : BaseFragment<HomeView, HomePresenter>(), HomeView {
 
     override fun init() {
+//        rv_1.adapter =
     }
 
     override fun initData() {
